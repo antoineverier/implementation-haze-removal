ee4c782a517a3be36bcf4b1d0456a95cabf91d42
Joseph Maillard <josephmaillard@MacBook-Air-de-Joseph.local>
2023-10-12 17:25:53 +0200

    test de guided_filter avec la transmission

--- test.py
+++ test.py
@@ -17,23 +17,30 @@ from cv2.ximgproc import guidedFilter
 im = skio.imread("haze_image/train.png")
 im2 = skio.imread("haze_image/trees-1587301_1280.jpg")
 
-darkchannel = dark_channel(im,9)
+darkchannel = dark_channel(im,15)
 
 image_gray = np.mean(im, axis=2).astype(np.uint8)
 
 lol = atmosphere_light(im, darkchannel, 0.001)
 
-t = transmission(im, lol, 0.95, 19)
+t = transmission(im, lol, 0.95, 15)
 
-radius = 50
-sigma = 0.00001
+radius = 80
+sigma = 0.015
 
 im = im.astype(np.float32)
+normI = (im - im.min()) / (im.max() - im.min())
 darkchannel = darkchannel.astype(np.float32)
 t = t.astype(np.float32)
-q = guidedFilter(im,darkchannel, radius, sigma)
-q2 = guidedFilter(im, t, radius, sigma)
+
+#t = (t - t.min()) / (t.max() - t.min())
+
+q = guidedFilter(normI,t, radius, sigma)
+#q2 = guidedFilter(im, t, radius, sigma)
 im = im.astype(np.uint8)
+
+#t = (t - t.min()) / (t.max() - t.min())
+#t = t.astype(np.uint8)
 
 
 
@@ -41,8 +48,8 @@ fig, axes = plt.subplots(1, 3, figsize=(25, 25))
 axes[0].imshow(im, cmap='gray')
 axes[0].set_title('Image original')
 axes[1].imshow(q, cmap='gray')
-axes[1].set_title("image filtré à l'aide de dark channel")
-axes[2].imshow(darkchannel, cmap='gray')
-axes[2].set_title('dark channel')
+axes[1].set_title("transmission filtré à partir de l'image originale")
+axes[2].imshow(t, cmap='gray')
+axes[2].set_title('transmission')
 
 plt.show()
